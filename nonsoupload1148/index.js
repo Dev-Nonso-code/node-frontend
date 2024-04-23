f13ec186cb886f97218b02c0a9cdcaefd384646a
Dev-Nonso-code <166655515+Dev-Nonso-code@users.noreply.github.com>
2024-04-23 15:54:52 +0100

    Add files via upload

---
+++ nonsoupload1148/index.js
@@ -0,0 +1,13 @@
+const majorOperations  = (a, b) =>  {
+    let addition = a + b;
+    let subtraction = a - b;
+    let mulit = a * b;
+    let duble = a ** b;
+    let division = a / b;
+    let nonso = obasi;
+    let year = new year()
+    let date = new Date ()
+    return [addition, subtraction, mulit, duble, division, nonso, year, date]
+}
+
+module.exports = majorOperations
